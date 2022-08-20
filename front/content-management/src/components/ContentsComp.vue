0b787da679e1e4d061fbc1f86c01588323e478ab
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-08-20 15:32:14 +0430

    feat: add content comp

---
+++ front/content-management/src/components/ContentsComp.vue
@@ -0,0 +1,451 @@
+<template>
+  <div>
+    <div class="sidebar">
+      <div class="logo-details">
+        <span class="logo-name">Content Management</span>
+        <span class="logo-icon">
+          <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" class="sidebar-icon-doc"/>
+        </span>
+      </div>
+      <ul class="nav-links">
+        <li :class="[showDocMenu ? 'showMenu': '']">
+          <div class="icon-link">
+            <font-awesome-icon icon="fa-solid fa-angle-up" class="sidebar-icon-doc"/>
+            <a class="sidebar-item-icon-doc">
+              <span>
+                <font-awesome-icon icon="fa-solid fa-file" class="sidebar-icon-doc"/>
+              </span>
+              <span class="link-name">Document</span>
+            </a>
+            <font-awesome-icon v-if="showDocMenu" icon="fa-solid fa-angle-up" class="arrow" @click="toggleDocSubMenu()"/>
+            <font-awesome-icon v-if="!showDocMenu" icon="fa-solid fa-angle-down" class="arrow" @click="toggleDocSubMenu()"/>
+          </div>
+          <ul class="sub-menu">
+            <li><a class="link-name" href="#">Document</a></li>
+            <li v-for="content in docContents" :key="content.id" >
+              <a>{{content.title}}</a>
+            </li>
+            <li>
+              <a>
+              <span>
+                  <font-awesome-icon icon="fa-solid fa-plus" class="sidebar-icon-add"/>
+              </span>
+              <span class="add-text" @click="newDoc()">
+                add new content
+              </span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li :class="[showAudioMenu ? 'showMenu': '']">
+          <div class="icon-link">
+            <font-awesome-icon icon="fa-solid fa-angle-up" class="sidebar-icon-audio"/>
+            <a class="sidebar-item-icon-audio">
+              <span>
+                <font-awesome-icon icon="fa-solid fa-volume-high" class="sidebar-icon-audio"/>
+              </span>              
+              <span class="link-name">Audio</span>
+            </a>
+            <font-awesome-icon v-if="showAudioMenu" icon="fa-solid fa-angle-up" class="arrow" @click="toggleAudioSubMenu()"/>
+            <font-awesome-icon v-if="!showAudioMenu" icon="fa-solid fa-angle-down" class="arrow" @click="toggleAudioSubMenu()"/>
+          </div>
+          <ul class="sub-menu">
+            <li><a class="link-name" href="#">Audio</a></li>
+            <li v-for="content in audioContents" :key="content.id" >
+              <a>{{content.title}}</a>
+            </li>
+            <li>
+              <a>
+              <span>
+                  <font-awesome-icon icon="fa-solid fa-plus" class="sidebar-icon-add"/>
+              </span>
+              <span class="add-text">
+                add new content
+              </span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li :class="[showVideoMenu ? 'showMenu': '']">
+          <div class="icon-link">
+          <font-awesome-icon icon="fa-solid fa-video" class="sidebar-icon-video"/>
+            <a class="sidebar-item-icon-video">
+              <span>
+                <font-awesome-icon icon="fa-solid fa-video" class="sidebar-icon-video"/>
+              </span>
+              
+              <span class="link-name">Video</span>
+            </a>
+            <font-awesome-icon v-if="showVideoMenu" icon="fa-solid fa-angle-up" class="arrow" @click="toggleVideoSubMenu()"/>
+            <font-awesome-icon v-if="!showVideoMenu" icon="fa-solid fa-angle-down" class="arrow" @click="toggleVideoSubMenu()"/>
+          </div>
+          <ul class="sub-menu">
+            <li><a class="link-name" href="#">Video</a></li>
+            <li v-for="content in videoContents" :key="content.id" >
+              <a>{{content.title}}</a>
+            </li>
+            <li>
+              <a>
+              <span>
+                  <font-awesome-icon icon="fa-solid fa-plus" class="sidebar-icon-add"/>
+              </span>
+              <span class="add-text">
+                add new content
+              </span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <section class="home-section">
+      <div class="home-content">
+        <new-doc-content v-if="showNewDocPage" />
+        <doc-contet />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import NewDocContent from './NewDocContent.vue'
+import DocContet from './DocContent.vue'
+
+export default {
+  components: {
+    NewDocContent,
+    DocContet,
+  },
+  data(){
+    return {
+      showDocMenu: false,
+      showAudioMenu: false,
+      showVideoMenu:false,
+      contents : [
+        {
+          "id": 1,
+          "title" : "doc1",
+          "category": "doc"
+        },
+        {
+          "id": 2,
+          "title" : "video1",
+          "category": "video"
+        },
+        {
+          "id": 3,
+          "title" : "audio1",
+          "category": "audio"
+        },
+        {
+          "id": 4,
+          "title" : "audio2",
+          "category": "audio"
+        },
+        {
+          "id": 5,
+          "title" : "video2",
+          "category": "video"
+        },
+                {
+          "id": 6,
+          "title" : "video3",
+          "category": "video"
+        },
+                {
+          "id": 7,
+          "title" : "video4",
+          "category": "video"
+        },
+                {
+          "id": 8,
+          "title" : "video5",
+          "category": "video"
+        },
+                {
+          "id": 9,
+          "title" : "video6",
+          "category": "video"
+        },
+                {
+          "id": 10,
+          "title" : "audio3",
+          "category": "audio"
+        },
+        {
+          "id": 11,
+          "title" : "doc2",
+          "category": "doc"
+        },
+        {
+          "id": 12,
+          "title" : "doc3",
+          "category": "doc"
+        }
+      ],
+      showNewDocPage: false,
+    }
+  },
+  computed: {
+    docContents() {
+      return this.contents.filter(item => item.category === 'doc')
+    },
+    audioContents() {
+      return this.contents.filter(item => item.category === 'audio')
+    },
+    videoContents() {
+      return this.contents.filter(item => item.category === 'video')
+    },
+  },
+  methods: {
+    toggleDocSubMenu(){
+      this.showDocMenu = ~this.showDocMenu;
+    },
+    toggleAudioSubMenu(){
+      this.showAudioMenu = ~this.showAudioMenu;
+    },
+    toggleVideoSubMenu(){
+      this.showVideoMenu = ~this.showVideoMenu;
+    },
+    newDoc(){
+      this.showNewDocPage = true;
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.contents-container {
+  position:absolute;
+  top:0px;
+  right:0px;
+  bottom:0px;
+  left:0px;
+}
+.contents-sidebar{
+  background-color: #080710 ;
+  height: 100%;
+  overflow-y: scroll;
+}
+.content-container{
+  background-color: rgba(255,255,255,0.13); ;
+  height: 100%;
+  overflow-y: scroll;
+}
+.content-title{
+  height: 80px;
+  margin-top: 10px;
+  color: white;
+  text-align: left;
+  border-radius: 10px;
+  backdrop-filter: blur(10px);
+  border-bottom: 2px solid rgba(255,255,255,0.1);
+  box-shadow: 0 0 40px rgba(8,7,16,0.6);
+  padding: 8px;
+  background-color: rgba(255,255,255,0.13);
+}
+.category-title {
+  color: #961540;
+  font-size: 20px;
+  padding-top: 10px;
+}
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
+.sidebar {
+  background: #080710;
+  width: 350px;
+  position:absolute;
+  top:0px;
+  right:0px;
+  bottom:0px;
+  left:0px;
+  height: 100%;
+  overflow-y: scroll;
+}
+.sidebar .logo-details {
+  height: 80px;
+  width: 100%;
+  min-width: 78px;
+  display: flex;
+  align-items: center;
+  font-size: 32px;
+  text-align: center;
+  line-height: 50px;
+}
+.sidebar .logo-details i {
+  font-size: 30px;
+  color: #fff;
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+}
+.sidebar .logo-details .logo-name {
+  font-size: 22px;
+  color: #fff;
+  font-weight: 600;
+  transition: 0.3s ease;
+  transition-delay: 0.1s;
+  margin-left: 30px;
+}
+.sidebar .nav-links {
+  height: 100%;
+  margin-left: -70px;
+  overflow: auto;
+}
+.arrow{
+  margin-right: 30px;
+  color: white  !important;
+}
+.sidebar .nav-links::-webkit-scrollbar {
+  display: none;
+}
+.sidebar .nav-links li {
+  position: relative;
+  list-style: none;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li:hover {
+  background: #1b1b31;
+}
+.sidebar .nav-links li:hover .sub-menu.blank {
+  top: 50%;
+  transform: translateY(-50%);
+}
+.sidebar .nav-links li.showMenu i.arrow {
+  transform: rotate(-180deg);
+}
+.sidebar .nav-links li.showMenu .sub-menu {
+  display: block;
+}
+.sidebar .nav-links li .icon-link {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.sidebar .nav-links li i {
+  height: 50px;
+  min-width: 78px;
+  text-align: center;
+  line-height: 50px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+.sidebar .nav-links li a {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+.sidebar .nav-links li a .link-name {
+  font-size: 20px;
+  font-weight: 400;
+  color: #fff;
+  transition: all 0.4s ease;
+}
+.sidebar .nav-links li .sub-menu {
+  padding: 6px 6px 14px 80px;
+  display: none;
+}
+.sidebar .nav-links li .sub-menu.blank {
+  opacity: 0;
+  pointer-events: none;
+  padding: 3px 20px 6px 16px;
+}
+.sidebar .nav-links li .sub-menu a {
+  color: #fff;
+  font-size: 18px;
+  padding: 5px 0;
+  white-space: nwrap;
+  opacity: 0.6;
+  transition: all 0.3 ease;
+}
+.sidebar .nav-links li .sub-menu a:hover {
+  opacity: 1;
+}
+.sidebar .nav-links li .sub-menu .link-name {
+  display: none;
+}
+.home-section {
+  position: absolute;
+  background: 	#dddee0;
+  height: 100vh;
+  left: 350px;
+  top: 0px;
+  width: calc(100% - 250px);
+  transition: all 0.5s ease;
+  overflow-y: scroll;
+}
+.home-section .home-content {
+  height: 60px;
+  display: flex;
+  align-items: center;
+}
+.home-section .home-content .bx-menu {
+  color: #11101d;
+  font-size: 35px;
+  margin: 0 35px;
+  cursor: pointer;
+}
+.home-section .home-content .text {
+  color: #11101d;
+  font-size: 26px;
+  font-weight: 600;
+}
+a:hover {
+  cursor: pointer;
+}
+.arrow:hover {
+  cursor: pointer;
+}
+.sidebar-icon-doc {
+  color: white;
+  margin-right: 30px;
+}
+.sidebar-item-icon-doc {
+  margin-left: -100px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.sidebar-icon-audio {
+  color: white;
+  margin-right: 27px;
+}
+.sidebar-item-icon-audio {
+  margin-left: -140px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.sidebar-icon-video {
+  color: white;
+  margin-right: 28px;
+}
+.sidebar-item-icon-video {
+  margin-left: -150px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.sidebar-icon-add {
+  color: white; 
+  opacity: 1;
+  cursor: pointer; 
+}
+.add-sidebar-section{
+  text-align: left;
+  margin-top: 10px;
+}
+.add-text {
+  margin-left: 10px;
+  color: #fff; 
+  opacity: 1;
+  cursor: pointer; 
+}
+.add-sidebar-section:hover {
+  opacity: 1; 
+}
+.logo-icon {
+  margin-left: 40px;
+  font-size: 20px;
+}
+</style>
