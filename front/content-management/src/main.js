f37222949be1ae89407769ca261177ad89480755
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-08-20 15:33:10 +0430

    enhancement: enhance json

--- front/content-management/src/main.js
+++ front/content-management/src/main.js
@@ -4,11 +4,33 @@ import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+import { faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
+
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+library.add(faAngleDown);
+library.add(faAngleUp);
+library.add(faFile);
+library.add(faVolumeHigh);
+library.add(faVideo);
+library.add(faPlus);
+library.add(faMinus);
+library.add(faArrowRightFromBracket);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 new Vue({
   router,
--- front/content-management/src/router/index.js
+++ front/content-management/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/RegisterPage.vue'
-
+import ContentsPage from '../views/ContentsPage.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +16,16 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: RegisterPage,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage,
+  },
+  {
+    path: '/contents',
+    name: 'Contents',
+    component: ContentsPage,
   }
 ]
 
