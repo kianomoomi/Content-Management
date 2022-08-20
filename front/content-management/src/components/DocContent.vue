5780f0b34e5875e62aea767f6fec365edc1ab17f
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-08-20 19:47:15 +0430

    feat: working with file downloader

--- front/content-management/src/components/DocContent.vue
+++ front/content-management/src/components/DocContent.vue
@@ -1,36 +1,15 @@
 <template>
   <div class="container">
-  <h2>Responsive Tables Using LI </h2>
+  <h2>{{title}}</h2>
+    <a href="title_1_A_Untitled_Document.pdf" download>Download</a>
   <ul class="responsive-table">
     <li class="table-header">
-      <div class="col col-1">Job Id</div>
-      <div class="col col-2">Customer Name</div>
-      <div class="col col-3">Amount Due</div>
-      <div class="col col-4">Payment Status</div>
+      <div class="col col-1">Field</div>
+      <div class="col col-2">Value</div>
     </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42235</div>
-      <div class="col col-2" data-label="Customer Name">John Doe</div>
-      <div class="col col-3" data-label="Amount">$350</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42442</div>
-      <div class="col col-2" data-label="Customer Name">Jennifer Smith</div>
-      <div class="col col-3" data-label="Amount">$220</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42257</div>
-      <div class="col col-2" data-label="Customer Name">John Smith</div>
-      <div class="col col-3" data-label="Amount">$341</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Job Id">42311</div>
-      <div class="col col-2" data-label="Customer Name">John Carpenter</div>
-      <div class="col col-3" data-label="Amount">$115</div>
-      <div class="col col-4" data-label="Payment Status">Pending</div>
+    <li v-for="(k, v) in extra" :key="k" class="table-row">
+      <div class="col col-1">{{k}}</div>
+      <div class="col col-2"> {{v}}</div>
     </li>
   </ul>
 </div>
@@ -38,19 +17,36 @@
 
 <script>
 export default {
+    props: {
+        title: String,
+    },
+    data(){
+        return{
+            extra: {
+            "f2": "v2",
+            "f1": "v1",
+            "f3": "v1",
+            "f4": "v1",
+            "f5": "v1",
+            "f6": "v1",
+            "f7": "v1",
+            "f8": "v1",
+
+            }
+        }
+    }
 
 }
 </script>
 
 <style scoped>
 .container {
-  margin-top: 500px;
+  margin-top: 100px;
   max-width: 1000px;
   padding-left: 10px;
   padding-right: 10px;
-  margin-left: 30px;
+  margin-left: 65px;
 }
-
 h2 {
   font-size: 26px;
   margin: 20px 0;
@@ -59,19 +55,22 @@ h2 {
 h2 small {
   font-size: 0.5em;
 }
-
+.responsive-table {
+    height: 50px;
+}
 .responsive-table li {
   border-radius: 3px;
-  padding: 25px 30px;
+  padding: 25px 25px;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
 }
 .responsive-table .table-header {
-  background-color: #95A5A6;
+  background-color: #080710;
   font-size: 14px;
   text-transform: uppercase;
   letter-spacing: 0.03em;
+  color: #fff;
 }
 .responsive-table .table-row {
   background-color: #ffffff;
@@ -81,7 +80,7 @@ h2 small {
   flex-basis: 10%;
 }
 .responsive-table .col-2 {
-  flex-basis: 40%;
+  flex-basis: 90%;
 }
 .responsive-table .col-3 {
   flex-basis: 25%;
